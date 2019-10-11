23849a4aac4bad64dea623d18527b0477299201a
s-volod <make.m.btw@gmail.com>
2019-10-11 16:41:25 +0000

    node

--- js/index.js
+++ js/index.js
@@ -1,26 +1,33 @@
-document.querySelector('.page-loaded').innerText = new Date().toLocaleTimeString();
+document.querySelector('.page-loaded')
+    .innerText = new Date().toLocaleTimeString();
 
-document.querySelector('.ajax-get-html').addEventListener('click', getAjaxHtml);
+document.querySelector('.ajax-get-html')
+    .addEventListener('click', getAjaxHtml);
 
 function getAjaxHtml() {
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            document.querySelector('.html-container').innerHTML = xhr.responseText;
+            document.querySelector('.html-container')
+                .innerHTML = xhr.responseText;
         }
     }
     xhr.open('GET', 'client-data.html', true);
     xhr.send();
 }
 
-document.querySelector('.fetch-html').addEventListener('click', fetchHtml);
+document.querySelector('.fetch-html')
+    .addEventListener('click', fetchHtml);
 
 function fetchHtml() {
-    fetch('client-data.html').then(response=>response.text()).then(html=>document.querySelector('.html-container').innerHTML = html);
+    fetch('client-data.html')
+        .then( response => response.text() )
+        .then( html => document.querySelector('.html-container').innerHTML = html );
 }
 
 // JSON
-document.querySelector('.ajax-get-json').addEventListener('click', getAjaxJson);
+document.querySelector('.ajax-get-json')
+    .addEventListener('click', getAjaxJson);
 
 function getAjaxJson() {
     const xhr = new XMLHttpRequest();
@@ -35,12 +42,35 @@ function getAjaxJson() {
     xhr.send();
 }
 
-document.querySelector('.fetch-json').addEventListener('click', fetchJson);
+document.querySelector('.fetch-json')
+    .addEventListener('click', fetchJson);
 
 function fetchJson() {
-    fetch('client-data.json').then(response=>response.json()).then(clientData=>{
-        document.querySelector('.client-name').innerText = clientData.name;
-        document.querySelector('.account-balance').innerText = clientData.balance;
-    }
-    );
+    fetch('client-data.json')
+        .then( response => response.json() )
+        .then( clientData => {
+            document.querySelector('.client-name').innerText = clientData.name;
+            document.querySelector('.account-balance').innerText = clientData.balance;
+        } );
+}
+
+
+// Form with backend handler
+document.querySelector('.login-form input[type=submit]')
+    .addEventListener('click', login);
+
+function login(e) {
+    e.preventDefault();
+    fetch('login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({
+            name: document.querySelector('.login-form input[name=name]').value,
+            password: document.querySelector('.login-form input[name=password]').value
+        })
+    })
+        .then(_ => document.querySelector('.login-form').reset());
 }
